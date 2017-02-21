7747de8402a5b4219c7c92d51b5517dd94da16e5
Raphael Pampolha <ralepa@gmail.com>
2017-02-21 00:14:50 -0300

    Adicionando classe que guardará informações de temperatura de cidade e classe de teste básico com teste de construtor

---
+++ StoneClimaTempo.Tests/CityTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StoneClimaTempo.Tests
+{
+    /// <summary>
+    /// Classe de teste para City e suas funções
+    /// </summary>
+    [TestClass]
+    public class CityTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void CityShouldOnlyConstructWithName()
+        {
+            Models.City cityTest = new Models.City("Rio de Janeiro");
+            Assert.IsNotNull(cityTest);
+
+            string cityName = cityTest.Name;
+            Assert.IsNotNull(cityName);
+        }
+    }
+}
---
+++ StoneClimaTempo/Models/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoneClimaTempo.Models
+{
+    public class City
+    {
+
+        public City(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public IDictionary<DateTime, double> Temperatures { get; private set; }
+
+
+        public void AddTemperatureRegistry(DateTime dateTime, double temp)
+        {
+            Temperatures.Add(dateTime, temp);
+        }
+
+        public void ResetRegistries()
+        {
+            Temperatures.Clear();
+        }
+
+    }
+}
