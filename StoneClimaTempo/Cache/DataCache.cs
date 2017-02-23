31c18193e1fe94047a6eeae5393241c9f415ec8b
Raphael Pampolha <ralepa@gmail.com>
2017-02-23 00:21:57 -0300

    Adicionando classe de cache para guardar dados e criando classe de testes

---
+++ StoneClimaTempo/Cache/DataCache.cs
@@ -0,0 +1,36 @@
+﻿using StoneClimaTempo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoneClimaTempo.Cache
+{
+    /// <summary>
+    /// Singleton de cache para guardar os dados sobre as temperaturas, funcionando como um in-memory database
+    /// </summary>
+    public class DataCache
+    {
+        private static DataCache instance;
+
+        public List<CityTemperatures> cities { get; private set; }
+
+        private DataCache()
+        {
+            cities = new List<CityTemperatures>();
+        }
+
+        public static DataCache Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataCache();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
